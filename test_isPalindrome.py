0de92d0c66066b0acccdeaaa4b994a462a7c17d1
luisadrian17000 <luisadriandsg@gmail.com>
2026-03-27 12:47:30 -0600

    ejercicio 1: max num y tarea: is palindrome, cada uno con 6 test cases

---
+++ test_isPalindrome.py
@@ -0,0 +1,41 @@
+def is_palindrome(s: str) -> bool:
+
+    if s == "":
+        return None
+
+
+    i = 0
+    j = len(s) - 1
+
+    while i < j:
+        if s[i] != s[j]:
+            return False
+        i += 1
+        j -= 1
+    
+    if is_palindrome:
+        return True 
+
+
+
+def test_string():
+    assert is_palindrome("aeiouoiea") == True
+    
+
+def test_empty_string():
+    assert is_palindrome("") is None
+
+
+def test_palindrome_number():
+    assert is_palindrome("0000") == True
+
+
+
+def test_is_not_palindrome():
+    assert is_palindrome("8548745237984598494392022") == False
+
+
+
+
+
+
